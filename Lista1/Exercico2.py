861f0198550f2616b49dec862239f7881c156cb6
victorkleemann <162629302+victorkleemann@users.noreply.github.com>
2024-03-08 20:54:50 -0300

    Create Exercico2.py

---
+++ Lista1/Exercico2.py
@@ -0,0 +1,5 @@
+#Escreva um programa em linguagem Python que solicite o nome do usuário e, em seguida, imprima uma mensagem de boas-vindas na tela, utilizando o nome fornecido.
+
+nome = input('Qual é o seu nome? ')
+
+print('Olá',nome+', seja bem vindo!')
