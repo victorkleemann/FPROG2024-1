eb786a18647f512782de60c60496612b7713a255
victorkleemann <162629302+victorkleemann@users.noreply.github.com>
2024-03-13 22:00:05 -0300

    Create new 1.py

---
+++ Anotações/new 1.py
@@ -0,0 +1,11 @@
+#exibir a mensgaem da questão 3
+
+print('Qual é o verdadeiro nome do super-herói Batman?')
+print('a) Bruce Wayne') 
+print('b) Clark Kent') 
+print('b) Peter Parker') 
+print('d) Tony Stark') 
+print('e) Steve Rogers')
+
+Qual é o verdadeiro nome do super-herói Batman?
+a) Bruce Wayne \n b) Clark Kent\n c) Peter Parker \n d) Tony Stark \n e) Steve Rogers
