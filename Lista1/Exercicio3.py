6a59d1f95914e4ca7dd791c73bae5d4c7e43b124
victorkleemann <162629302+victorkleemann@users.noreply.github.com>
2024-03-13 21:17:04 -0300

    Adicionado Ex3 e Ex4

---
+++ Lista1/Exercicio3.py
@@ -0,0 +1,18 @@
+# exibir a mensgaem da questão 3
+
+print('Qual é o verdadeiro nome do super-herói Batman?')
+print('a) Bruce Wayne') 
+print('b) Clark Kent') 
+print('b) Peter Parker') 
+print('d) Tony Stark') 
+print('e) Steve Rogers')
+print()
+
+# armazenar a resposta correta em uma variavel
+rightAnswer = 'a'
+
+# perguntar a resposta do usuario e guardar em uma variavel
+userAnswer = input('Digite sua resposta: ')
+
+# Mensgame informando as respostas
+print('A sua resposta foi:',userAnswer,'\n A resposta correta é:',rightAnswer)
---
+++ Lista1/Exercicio4.py
@@ -0,0 +1,25 @@
+answer = input('Qual é a sua pergunta? ')
+print('Agora liste as alternativas desjeadas')
+answerA = input('Digite a alternativa A:')
+answerB = input('Digite a alternativa B:')
+answerC = input('Digite a alternativa C:')
+answerD = input('Digite a alternativa D:')
+answerE = input('Digite a alternativa E:')
+
+rightAnswer = input('Qual alternativa correta? ')
+
+print(answer)
+print('')
+print('a) ',answerA)
+print('b) ',answerB)
+print('c) ',answerC)
+print('d) ',answerD)
+print('e) ',answerE)
+
+userAnswer = input('Qual alternativa é a correta? ')
+print('A sua resposta foi:',userAnswer,'\nA resposta correta é:',rightAnswer)
+
+
+
+
+
